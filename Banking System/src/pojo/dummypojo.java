64b2618f2d4dc8dcef31173a848d3939bbf8df48
ArunPandian2001 <arunpandian253@gmail.com>
2022-10-22 17:45:23 +0530

    commit

---
+++ Banking System/src/pojo/dummypojo.java
@@ -0,0 +1,14 @@
+package pojo;
+
+public class dummypojo {
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private String name;
+}
